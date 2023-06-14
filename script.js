80729a92299cac27a4cca9fa490d5ce8342c3260
Agostina Ramos <110774783+Agossramos@users.noreply.github.com>
2023-06-14 11:18:46 -0300

    agregue funcion calcular

--- script.js
+++ script.js
@@ -3,4 +3,9 @@ function calcular() {
     // recuperar los valores de la página
     // sumarlos
     // agregarlos en la página en el lugar adecuado
+    var op1=document.getElementById("op1").value;
+    var op2=document.getElementById("op2").value;
+    var resultado = parseInt(op1)+parseInt(op2);
+    document.getElementById("resultado").innerHTML=resultado;
+
 }
